e3356bbe9bd20f99063e00b83d733efc882a9176
Reetta <reetta.sainio@helsinki.fi>
2021-03-28 20:04:43 +0300

    Sudokun rakenteen alustusta

---
+++ Sudoku/src/main/java/sudoku/ui/SudokuUi.java
@@ -0,0 +1,13 @@
+package sudoku.ui;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+
+public class SudokuUi extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        
+    }
+}
