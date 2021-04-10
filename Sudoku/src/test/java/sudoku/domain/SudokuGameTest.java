30be1f4fa09b0f7888c4834e3529cc0b4bda1a96
Reetta Sainio <reetta.sainio@helsinki.fi>
2021-04-10 17:08:34 +0300

    Added tests for SudokuGame

--- Sudoku/src/test/java/sudoku/domain/SudokuGameTest.java
+++ Sudoku/src/test/java/sudoku/domain/SudokuGameTest.java
@@ -5,10 +5,7 @@
  */
 package sudoku.domain;
 
-import org.junit.After;
-import org.junit.AfterClass;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
@@ -17,29 +14,27 @@ import static org.junit.Assert.*;
  * @author sareetta
  */
 public class SudokuGameTest {
-    
-    public SudokuGameTest() {
-    }
-    
-    @BeforeClass
-    public static void setUpClass() {
-    }
-    
-    @AfterClass
-    public static void tearDownClass() {
-    }
+    SudokuGame sudoku;
     
     @Before
     public void setUp() {
+        sudoku = new SudokuGame();
     }
     
-    @After
-    public void tearDown() {
+    @Test
+    public void constructorCreatesNewSudoku() {
+        assertTrue(sudoku != null);
     }
-
-    // TODO add test methods here.
-    // The methods must be annotated with annotation @Test. For example:
-    //
-    // @Test
-    // public void hello() {}
+    
+    @Test
+    public void newSudokuSetsBoardWithCorrectHight() {
+        assertTrue(9 == sudoku.getSudoku().length);
+    }
+    
+    @Test
+    public void newSudokuSetsBoardWithCorrectWidht() {
+        assertTrue(9 == sudoku.getSudoku()[0].length);
+    }
+    
+   
 }
