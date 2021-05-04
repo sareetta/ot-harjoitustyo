d3e1f4041b8e42b513c1c193e786b85d4a12c2fe
Reetta Sainio <reetta.sainio@helsinki.fi>
2021-05-04 19:43:13 +0300

    changes

--- Sudoku/src/main/java/sudoku/ui/SudokuUi.java
+++ Sudoku/src/main/java/sudoku/ui/SudokuUi.java
@@ -344,7 +344,7 @@ public class SudokuUi extends Application {
                 if (sudoku.getDifficulty() == 35) {
                     scoreDao.create(new SudokuScore(0, nameArea.getText(), time.Time()), "Medium");
                 } else if (sudoku.getDifficulty() == 25) {
-                    scoreDao.create(new SudokuScore(0, nameArea.getText(), time.Time()), "Easy");
+                  //  scoreDao.create(new SudokuScore(0, nameArea.getText(), time.Time()), "Easy");
                 }
                 
                 nameArea.setText("");
@@ -363,7 +363,7 @@ public class SudokuUi extends Application {
             mediumScoresList.getChildren().add(mediumTitle);
             easyScoresList.getChildren().add(easyTitle);
             List<SudokuScore> scoresMedium = scoreDao.list("Medium");
-            List<SudokuScore> scoresEasy = scoreDao.list("Easy");
+          //  List<SudokuScore> scoresEasy = scoreDao.list("Easy");
             
             if (scoresMedium.isEmpty()) {
                 Label noScores = new Label("No scores!");
@@ -380,7 +380,7 @@ public class SudokuUi extends Application {
                 }
             }
             
-            if (scoresEasy.isEmpty()) {
+       /*     if (scoresEasy.isEmpty()) {
                 Label noScores = new Label("No scores!");
                 noScores.setFont(Font.font("Lucida Sans Unicode", 20));
                 easyScoresList.getChildren().add(noScores);
@@ -394,7 +394,7 @@ public class SudokuUi extends Application {
                     easyScoresList.getChildren().add(newRecord);
                 }
             }
-            
+            */
             stage.setScene(statisticsScene);
         }); 
         
